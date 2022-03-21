686c1d651e0d24848de4feb8d4a7873a9b91f45f
Angelica <Angelica137@users.noreply.github.com>
2022-03-21 21:06:34 +0000

    test convert_coordinate(), red

--- tests/test_tuples.py
+++ tests/test_tuples.py
@@ -3,3 +3,7 @@ from scripts.tuples import *
 
 def test_get_coordinate():
     assert get_coordinate(("Scrimshaw Whale's Tooth", "2A")) == "2A"
+
+
+def test_convert_coordinate():
+    assert convert_coordinate("2A") == ("2", "A")
