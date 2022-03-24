c662e138b64f5fb9c71fa182e580ca9067454005
Angelica <Angelica137@users.noreply.github.com>
2022-03-24 20:03:13 +0000

    add compare_records(), green

--- scripts/tuples.py
+++ scripts/tuples.py
@@ -10,3 +10,11 @@ def convert_coordinate(coordinate: str) -> tuple:
     :return: tuple containing a number-letter pair from coordinate
     """
     return tuple(coordinate)
+
+
+def compare_records(list_one_info: tuple, list_two_info: tuple) -> bool:
+    """
+    Checks if coordinates in each tuple match
+    """
+    list_one_coordinate = convert_coordinate(get_coordinate(list_one_info))
+    return list_one_coordinate in list_two_info
