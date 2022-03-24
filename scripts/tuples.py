6267613d01e28007343d0246139db14bc3e33333
Angelica <Angelica137@users.noreply.github.com>
2022-03-24 20:25:05 +0000

    refactor compare_records()

--- scripts/tuples.py
+++ scripts/tuples.py
@@ -12,9 +12,9 @@ def convert_coordinate(coordinate: str) -> tuple:
     return tuple(coordinate)
 
 
-def compare_records(list_one_info: tuple, list_two_info: tuple) -> bool:
+def compare_records(list_one_item: tuple, list_two_item: tuple) -> bool:
     """
     Checks if coordinates in each tuple match
     """
-    list_one_coordinate = convert_coordinate(get_coordinate(list_one_info))
-    return list_one_coordinate in list_two_info
+    list_one_coordinate = convert_coordinate(get_coordinate(list_one_item))
+    return list_one_coordinate in list_two_item
