a1b1f784aaeb884e0e9fe6661a33ce59ac849e14
Angelica <Angelica137@users.noreply.github.com>
2022-03-24 20:22:19 +0000

    add new test compare_records(), green

--- tests/test_tuples.py
+++ tests/test_tuples.py
@@ -13,3 +13,9 @@ def test_compare_records():
     list_one_info = ('Brass Spyglass', '4B')
     list_two_info = ('Seaside Cottages', ('1', 'C'), 'blue')
     assert compare_records(list_one_info, list_two_info) == False
+
+
+def test_compare_records_True():
+    list_one_info = ('Model Ship in Large Bottle', '8A')
+    list_two_info = ('Harbor Managers Office', ('8', 'A'), 'purple')
+    assert compare_records(list_one_info, list_two_info) == True
