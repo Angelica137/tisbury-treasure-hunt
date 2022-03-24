78e8be34fae75aee1683661e377eaa58272c306e
Angelica <Angelica137@users.noreply.github.com>
2022-03-24 19:54:07 +0000

    add docstring to get_coordinate()

--- scripts/tuples.py
+++ scripts/tuples.py
@@ -1,5 +1,8 @@
-def get_coordinate(record: tuple) -> str:
-    return record[-1]
+def get_coordinate(item: tuple) -> str:
+    """
+    :return: the map coordinate from the treasure, coordinate tuple
+    """
+    return item[-1]
 
 
 def convert_coordinate(coordinate: str) -> tuple:
