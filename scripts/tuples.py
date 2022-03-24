e08acb7e11afb459085b2a4d6b68ecce5b7516c5
Angelica <Angelica137@users.noreply.github.com>
2022-03-24 20:36:04 +0000

    add create_record(), green

--- scripts/tuples.py
+++ scripts/tuples.py
@@ -18,3 +18,13 @@ def compare_records(list_one_item: tuple, list_two_item: tuple) -> bool:
     """
     list_one_coordinate = convert_coordinate(get_coordinate(list_one_item))
     return list_one_coordinate in list_two_item
+
+
+def create_record(list_one_item: tuple, list_two_item: tuple) -> tuple:
+    """
+    Checks if coordinates match for the items passed
+    If they do, returns tuple with all info from both parameters
+    If not, returns "not a match"
+    """
+    if compare_records(list_one_item, list_two_item):
+        return list_one_item + list_two_item
